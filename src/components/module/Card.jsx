468f219232241e3c9bcb73b9ba0fa0e1a9112636
muhislah <el.islah.udin@gmail.com>
2023-05-10 23:24:21 +0700

    first commit

---
+++ src/components/module/Card.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { View, Text, StyleSheet, Image, Button, TouchableOpacity } from 'react-native'
+
+const Card = ({ url, id, like, handleLike, handleDislike }) => {
+
+    return (
+        <View style={styles.card}>
+            <Image style={styles.image} source={{ uri: url }} alt={url} />
+            <View style={styles.actionContainer}>
+                <View style={styles.chip}>
+                    <Text>{like} Likes</Text>
+                </View>
+                <View style={[styles.actionContainer, { marginLeft: 'auto' }]}>
+                    <TouchableOpacity style={[styles.button, { backgroundColor: '#0384D8' }]} onPress={() => handleLike(id)} >
+                        <Text style={{ color: 'white' }}>Like</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.button, { backgroundColor: '#D82A03' }]} onPress={() => handleDislike(id)} >
+                        <Text style={{ color: 'white' }}>Dislike</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    card: {
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#eee',
+        padding: 10,
+    },
+    actionContainer: {
+        flexDirection: 'row',
+        gap: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button: {
+        paddingHorizontal: 20,
+        paddingVertical: 5,
+        borderRadius: 3
+    },
+    chip: {
+        paddingHorizontal: 20,
+        paddingVertical: 5,
+        borderRadius: 3,
+        borderWidth: 1,
+        borderColor: '#ababab'
+    },
+    image: {
+        width: '100%',
+        height: 200,
+        borderRadius: 5,
+        marginBottom: 10
+    }
+})
+
+export default Card
---
+++ src/screen/Home.jsx
@@ -0,0 +1,128 @@
+import React, { useState } from 'react'
+import jsonData from '../assets/data.json'
+import { SafeAreaView, ScrollView, View, Text, StyleSheet, Pressable, TouchableHighlight, TouchableOpacity } from 'react-native'
+import Card from '../components/module/Card'
+
+const Home = () => {
+    const [data, setData] = useState(jsonData)
+
+    const handleLike = (id) => {
+        setData(prev => {
+            let newData = prev.map((data) => {
+                if (data.id === id) {
+                    return {
+                        ...data,
+                        like: data.like++
+                    }
+                } else {
+                    return data
+                }
+            })
+
+            return newData
+        })
+    }
+
+    const handleDislike = (id) => {
+        setData(prev => {
+            let newData = prev.map((data) => {
+                if (data.id === id) {
+                    return {
+                        ...data,
+                        like: data.like--
+                    }
+                } else {
+                    return data
+                }
+            })
+
+            return newData
+        })
+    }
+
+    const handleReset = () => {
+        const newData = data.map((data) => {
+            return {
+                ...data,
+                like: 0
+            }
+        })
+        setData(newData)
+    }
+
+    const handleLikeAll = () => {
+        const newData = data.map((data) => {
+            return {
+                ...data,
+                like: data.like++
+            }
+        })
+        setData(newData)
+    }
+
+    const handleDislikeAll = () => {
+        const newData = data.map((data) => {
+            return {
+                ...data,
+                like: data.like--
+            }
+        })
+        setData(newData)
+    }
+
+    return (
+        <SafeAreaView>
+            <ScrollView >
+                <View style={styles.container}>
+                    <View style={styles.actionContainer}>
+                        <TouchableOpacity style={[styles.button, { backgroundColor: '#0384D8' }]} onPress={handleLikeAll} >
+                            <Text style={{ color: 'white' }}>Like All</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={[styles.button, { backgroundColor: 'white', borderWidth: 1, borderColor: '#ababab' }]} onPress={handleReset} >
+                            <Text style={{ color: '#ababab' }}>Reset All</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={[styles.button, { backgroundColor: '#D82A03' }]} onPress={handleDislikeAll} >
+                            <Text style={{ color: 'white' }}>Dislike All</Text>
+                        </TouchableOpacity>
+                    </View>
+                    {
+                        data && data.length ?
+                            data.map((data) => {
+                                return (
+                                    <Card
+                                        key={data.id}
+                                        url={data.url}
+                                        id={data.id}
+                                        handleLike={handleLike}
+                                        handleDislike={handleDislike}
+                                        like={data.like}
+                                    />
+                                )
+                            }) : null
+                    }
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+        gap: 10,
+    },
+    button: {
+        paddingHorizontal: 20,
+        paddingVertical: 5,
+        borderRadius: 3,
+        flex: 1,
+        textAlign: 'center',
+        alignItems: 'center'
+    },
+    actionContainer: {
+        flexDirection: 'row',
+        gap: 10
+    }
+})
+
+export default Home
